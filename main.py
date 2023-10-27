29d8aff593e139a29d54a7dbcb797d7554cc05ed
SpanishCat <ein.camonu@gmail.com>
2023-10-27 07:32:52 +0300

    Alternative search test

--- main.py
+++ main.py
@@ -12,20 +12,35 @@ import urllib.request
 import urllib.error
 
 from datetime import date, timedelta
-from typing import Final as Const
-
+from typing import Final as Const, Any
 
 # region Config
 # Output
 OUT_FORMAT: Const = "json"
-OUT_DIR: Const = "out/"
+OUT_DIR: Const = "out_old/"
 START_DAYS_AGO: Const = 4  # Update info for every day since x days ago
+RESULTS_PER_KEYWORD: Const = 3
 # START_DATE: Const = "20231001"
 
 # Misc
 UPDATE_TIME: Const = "12:00"
 KEYWORDS_FILENAME: Const = "keywords.txt"
-# end region
+
+
+# endregion
+
+
+class Article:
+    def __init__(self, id: int, title: str, key: str, url: str = ""):
+        self.id = id
+        self.title = title
+        self.key = key
+        self.url = url
+        self.language: str  # todo
+        self.matched_title: Any  # todo
+
+    def __repr__(self):
+        return self.title
 
 
 def to_timestamp(date: date):
@@ -81,21 +96,84 @@ def find_articles_by_keywords(keywords_: iter) -> tuple[wiki.WikipediaPage]:
     # Search keywords in Wikipedia
     article_titles = set()
     x = 0
-    for request_num, word in enumerate(keywords_):
-        article_titles.add(tuple(wiki.search(word, results=5)))
+
+    base_title_url = "https://api.wikimedia.org/core/v1/wikipedia/en/search/title"
+    base_content_url = "https://api.wikimedia.org/core/v1/wikipedia/en/search/page"
+    articles = []
+
+    for word_num, word in enumerate(keywords_):
+        article_titles.add(tuple(wiki.search(word, results=RESULTS_PER_KEYWORD)))
+
+        # title_search_results = (
+        #     requests.get(
+        #         base_title_url,
+        #         params={"q": word, "limit": RESULTS_PER_KEYWORD})
+        #         .json()
+        # )
+        # title_search_results = tuple(title_search_results["pages"]) \
+        #     if "pages" in title_search_results \
+        #     else tuple()
+        #
+        # content_search_results = (
+        #     requests.get(
+        #         base_content_url,
+        #         params={"q": word, "limit": RESULTS_PER_KEYWORD})
+        #         .json()
+        # )
+        # content_search_results = tuple(content_search_results["pages"]) \
+        #     if "pages" in content_search_results \
+        #     else tuple()
+        #
+        # if len(title_search_results) == 0 and len(content_search_results) == 0:
+        #     print("skipping")
+        #     continue
+        #
+        # print(f"Title: {tuple(x['title'] for x in title_search_results)}")
+        # print(f"Content: {tuple(x['title'] for x in content_search_results)}")
+        # new_pages = tuple(title_search_results) + tuple(content_search_results)
+        # print(f"{new_pages=}")
+        # new_pages = tuple(
+        #     page for page in new_pages
+        #     if page["key"] not in article_titles)
+        # print(f"{new_pages=}")
+        #
+        # if len(new_pages) == 0:
+        #     print("skipping")
+        #     continue
+        #
+        # new_pages = tuple(
+        #     Article(page["id"], page["title"], page["key"])
+        #     for page in new_pages)
+        # print(f"{new_pages=}")
+        #
+        # article_titles.add(page.key for page in new_pages)
+        # articles += [page.key for page in new_pages]
+
+
+        # print(f"new articles: {tuple(x['title'] for x in temp_new_articles)}")
+
+        # for page in article_request:
+        #     if page["key"] not in article_titles:
+        #         articles.add(page["key"])
+        #         article_titles.add(page["key"])
+
+        # tuple(
+        #     (articles.add(page["key"]), article_titles.add(page["key"]))
+        #     for page in title_search_results
+        #     if page["key"] not in article_titles
+        # )
 
         if x / len(keywords_) >= 0.01:
             x = 0
-            print(f"\r- Searching keywords in Wikipedia ({request_num}/{len(keywords_)}; "
-                  f"{int(request_num / len(keywords_) * 100)}%)",
+            print(f"\r- Searching keywords in Wikipedia ({word_num}/{len(keywords_)}; "
+                  f"{int(word_num / len(keywords_) * 100)}%)",
                   end="")
-
         x += 1
 
     article_titles = set(article for tup in article_titles for article in tup)
     print(f"\nArticle titles: {article_titles}\n\n")
 
-    # Convert Titles to 'WikipediaPage's (Title -> Page ID -> WikipediaPage)
+    # Convert Titles to 'WikipediaPage's (Title -> Page ID -> WikipediaPage):
     print("- Converting to 'wikipedia.WikipediaPage' class")
 
     # Titles -> Page IDs
